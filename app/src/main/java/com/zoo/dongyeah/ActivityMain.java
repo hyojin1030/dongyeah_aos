c351615b85fabbd2eea785aa31f8cebfa10259be
hyojin1030 <hyojin_030@naver.com>
2022-10-12 17:26:28 +0900

    지도 마커 추가

--- app/src/main/java/com/zoo/dongyeah/ActivityMain.java
+++ app/src/main/java/com/zoo/dongyeah/ActivityMain.java
@@ -5,295 +5,147 @@ import android.content.DialogInterface;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.Signature;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.util.Base64;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import net.daum.mf.map.api.CalloutBalloonAdapter;
+import net.daum.mf.map.api.CameraUpdateFactory;
 import net.daum.mf.map.api.MapLayout;
+import net.daum.mf.map.api.MapPOIItem;
 import net.daum.mf.map.api.MapPoint;
+import net.daum.mf.map.api.MapPointBounds;
 import net.daum.mf.map.api.MapView;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
-public class ActivityMain extends AppCompatActivity implements MapView.OpenAPIKeyAuthenticationResultListener, MapView.MapViewEventListener {
-    private static final int MENU_MAP_TYPE = Menu.FIRST + 1;
-    private static final int MENU_MAP_MOVE = Menu.FIRST + 2;
-
-    private static final String LOG_TAG = "MapViewDemoActivity";
+public class ActivityMain extends AppCompatActivity implements MapView.MapViewEventListener, MapView.POIItemEventListener {
+    private static final MapPoint CUSTOM_MARKER_POINT = MapPoint.mapPointWithGeoCoord(37.537229, 127.005515);
+    private static final MapPoint CUSTOM_MARKER_POINT2 = MapPoint.mapPointWithGeoCoord(37.447229, 127.015515);
+    private static final MapPoint DEFAULT_MARKER_POINT = MapPoint.mapPointWithGeoCoord(37.4020737, 127.1086766);
 
     private MapView mMapView;
+    private MapPOIItem mDefaultMarker;
+    private static final String LOG_TAG = "com.zoo.dongyeah";
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        getHashKey();
-
-        MapLayout mapLayout = new MapLayout(this);
-        mMapView = mapLayout.getMapView();
-
-
+        mMapView = (MapView) findViewById(R.id.map_view);
         mMapView.setDaumMapApiKey(BuildConfig.KAKAO_API_KEY);
-        mMapView.setOpenAPIKeyAuthenticationResultListener(this);
         mMapView.setMapViewEventListener(this);
-        mMapView.setMapType(MapView.MapType.Standard);
+        mMapView.setPOIItemEventListener(this);
 
-        ViewGroup mapViewContainer = (ViewGroup) findViewById(R.id.map_view);
-        mapViewContainer.addView(mapLayout);
+        createDefaultMarker(mMapView);
+        showAll();
 
-    }
 
-    private void getHashKey(){
-        PackageInfo packageInfo = null;
-        try {
-            packageInfo = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
-        } catch (PackageManager.NameNotFoundException e) {
-            e.printStackTrace();
-        }
-        if (packageInfo == null)
-            Log.e("KeyHash", "KeyHash:null");
+        mMapView.setCurrentLocationTrackingMode(MapView.CurrentLocationTrackingMode.TrackingModeOnWithoutHeading);
 
-        for (Signature signature : packageInfo.signatures) {
-            try {
-                MessageDigest md = MessageDigest.getInstance("SHA");
-                md.update(signature.toByteArray());
-                Log.d("KeyHash", Base64.encodeToString(md.digest(), Base64.DEFAULT));
-            } catch (NoSuchAlgorithmException e) {
-                Log.e("KeyHash", "Unable to get MessageDigest. signature=" + signature, e);
-            }
-        }
+
+
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        super.onCreateOptionsMenu(menu);
-        menu.add(0, MENU_MAP_TYPE, Menu.NONE, "MapType");
-        menu.add(0, MENU_MAP_MOVE, Menu.NONE, "Move");
-
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-
-        final int itemId = item.getItemId();
-
-        switch (itemId) {
-            case MENU_MAP_TYPE: {
-
-                //String hdMapTile = mMapView.isHDMapTileEnabled()? "HD Map Tile Off" : "HD Map Tile On";
-
-                String hdMapTile;
-
-                if (mMapView.getMapTileMode() == MapView.MapTileMode.HD2X) {
-                    hdMapTile = "Set to Standard Mode";
-                } else if (mMapView.getMapTileMode() == MapView.MapTileMode.HD) {
-                    hdMapTile = "Set to HD 2X Mode";
-                } else {
-                    hdMapTile = "Set to HD Mode";
-                }
-
-                String[] mapTypeMenuItems = { "Standard", "Satellite", "Hybrid", hdMapTile, "Clear Map Tile Cache"};
-
-                AlertDialog.Builder dialog = new AlertDialog.Builder(this);
-                dialog.setTitle("MapType");
-                dialog.setItems(mapTypeMenuItems, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        controlMapTile(which);
-                    }
-                });
-                dialog.show();
-
-
-                return true;
-            }
-
-            case MENU_MAP_MOVE: {
-                String rotateMapMenu = mMapView.getMapRotationAngle() == 0.0f? "Rotate Map 60" : "Unrotate Map";
-                String[] mapMoveMenuItems = { "Move to", "Zoom to", "Move and Zoom to", "Zoom In", "Zoom Out", rotateMapMenu};
-
-                AlertDialog.Builder dialog = new AlertDialog.Builder(this);
-                dialog.setTitle("Move");
-                dialog.setItems(mapMoveMenuItems, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        controlMapMove(which);
-                    }
-
-                });
-                dialog.show();
-
-                return true;
-            }
-        }
-
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    private void controlMapMove(int which) {
-        switch (which) {
-            case 0: // Move to
-            {
-                mMapView.setMapCenterPoint(MapPoint.mapPointWithGeoCoord(37.53737528, 127.00557633), true);
-            }
-            break;
-            case 1: // Zoom to
-            {
-                mMapView.setZoomLevel(7, true);
-            }
-            break;
-            case 2: // Move and Zoom to
-            {
-                mMapView.setMapCenterPointAndZoomLevel(MapPoint.mapPointWithGeoCoord(33.41, 126.52), 9, true);
-            }
-            break;
-            case 3: // Zoom In
-            {
-                mMapView.zoomIn(true);
-            }
-            break;
-            case 4: // Zoom Out
-            {
-                mMapView.zoomOut(true);
-            }
-            break;
-            case 5: // Rotate Map 60, Unrotate Map
-            {
-                if (mMapView.getMapRotationAngle() == 0.0f) {
-                    mMapView.setMapRotationAngle(60.0f, true);
-                } else {
-                    mMapView.setMapRotationAngle(0.0f, true);
-                }
-            }
-            break;
-        }
-    }
-
-    /**
-     * 지도 타일 컨트롤.
-     */
-    private void controlMapTile(int which) {
-        switch (which) {
-            case 0: // Standard
-            {
-                mMapView.setMapType(MapView.MapType.Standard);
-            }
-            break;
-            case 1: // Satellite
-            {
-                mMapView.setMapType(MapView.MapType.Satellite);
-            }
-            break;
-            case 2: // Hybrid
-            {
-                mMapView.setMapType(MapView.MapType.Hybrid);
-            }
-            break;
-            case 3: // HD Map Tile On/Off
-            {
-                if (mMapView.getMapTileMode() == MapView.MapTileMode.HD2X) {
-                    //Set to Standard Mode
-                    mMapView.setMapTileMode(MapView.MapTileMode.Standard);
-                } else if (mMapView.getMapTileMode() == MapView.MapTileMode.HD) {
-                    //Set to HD 2X Mode
-                    mMapView.setMapTileMode(MapView.MapTileMode.HD2X);
-                } else {
-                    //Set to HD Mode
-                    mMapView.setMapTileMode(MapView.MapTileMode.HD);
-                }
-            }
-            break;
-            case 4: // Clear Map Tile Cache
-            {
-                MapView.clearMapTilePersistentCache();
-            }
-            break;
-        }
-    }
-
-    //	/////////////////////////////////////////////////////////////////////////////////////////////////
-    // net.daum.mf.map.api.MapView.OpenAPIKeyAuthenticationResultListener
-
-    @Override
-    public void onDaumMapOpenAPIKeyAuthenticationResult(MapView mapView, int resultCode, String resultMessage) {
-        Log.i(LOG_TAG,	String.format("Open API Key Authentication Result : code=%d, message=%s", resultCode, resultMessage));
-    }
-
-    /////////////////////////////////////////////////////////////////////////////////////////////////
-    // net.daum.mf.map.api.MapView.MapViewEventListener
-
     public void onMapViewInitialized(MapView mapView) {
-        Log.i(LOG_TAG, "MapView had loaded. Now, MapView APIs could be called safely");
-        //mMapView.setCurrentLocationTrackingMode(MapView.CurrentLocationTrackingMode.TrackingModeOnWithoutHeading);
-        mapView.setMapCenterPointAndZoomLevel(MapPoint.mapPointWithGeoCoord(37.537229,127.005515), 2, true);
+
+    }
+
+
+    private void createDefaultMarker(MapView mapView) {
+        mDefaultMarker = new MapPOIItem();
+        String name = "Default Marker";
+        mDefaultMarker.setItemName(name);
+        mDefaultMarker.setTag(0);
+        mDefaultMarker.setMapPoint(DEFAULT_MARKER_POINT);
+        mDefaultMarker.setMarkerType(MapPOIItem.MarkerType.BluePin);
+        mDefaultMarker.setSelectedMarkerType(MapPOIItem.MarkerType.RedPin);
+
+        mapView.addPOIItem(mDefaultMarker);
+        mapView.selectPOIItem(mDefaultMarker, true);
+        mapView.setMapCenterPoint(DEFAULT_MARKER_POINT, false);
+    }
+
+    private void showAll() {
+        int padding = 20;
+        float minZoomLevel = 7;
+        float maxZoomLevel = 10;
+        MapPointBounds bounds = new MapPointBounds(CUSTOM_MARKER_POINT, DEFAULT_MARKER_POINT);
+        mMapView.moveCamera(CameraUpdateFactory.newMapPointBounds(bounds, padding, minZoomLevel, maxZoomLevel));
     }
 
     @Override
-    public void onMapViewCenterPointMoved(MapView mapView, MapPoint mapCenterPoint) {
-        MapPoint.GeoCoordinate mapPointGeo = mapCenterPoint.getMapPointGeoCoord();
-        Log.i(LOG_TAG, String.format("MapView onMapViewCenterPointMoved (%f,%f)", mapPointGeo.latitude, mapPointGeo.longitude));
+    public void onMapViewCenterPointMoved(MapView mapView, MapPoint mapPoint) {
+
+    }
+
+    @Override
+    public void onMapViewZoomLevelChanged(MapView mapView, int i) {
+
+    }
+
+    @Override
+    public void onMapViewSingleTapped(MapView mapView, MapPoint mapPoint) {
+
     }
 
     @Override
     public void onMapViewDoubleTapped(MapView mapView, MapPoint mapPoint) {
 
-        MapPoint.GeoCoordinate mapPointGeo = mapPoint.getMapPointGeoCoord();
-
-        AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
-        alertDialog.setTitle("DaumMapLibrarySample");
-        alertDialog.setMessage(String.format("Double-Tap on (%f,%f)", mapPointGeo.latitude, mapPointGeo.longitude));
-        alertDialog.setPositiveButton("OK", null);
-        alertDialog.show();
     }
 
     @Override
     public void onMapViewLongPressed(MapView mapView, MapPoint mapPoint) {
 
-        MapPoint.GeoCoordinate mapPointGeo = mapPoint.getMapPointGeoCoord();
-
-        AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
-        alertDialog.setTitle("DaumMapLibrarySample");
-        alertDialog.setMessage(String.format("Long-Press on (%f,%f)", mapPointGeo.latitude, mapPointGeo.longitude));
-        alertDialog.setPositiveButton("OK", null);
-        alertDialog.show();
-    }
-
-    @Override
-    public void onMapViewSingleTapped(MapView mapView, MapPoint mapPoint) {
-        MapPoint.GeoCoordinate mapPointGeo = mapPoint.getMapPointGeoCoord();
-        Log.i(LOG_TAG, String.format("MapView onMapViewSingleTapped (%f,%f)", mapPointGeo.latitude, mapPointGeo.longitude));
     }
 
     @Override
     public void onMapViewDragStarted(MapView mapView, MapPoint mapPoint) {
-        MapPoint.GeoCoordinate mapPointGeo = mapPoint.getMapPointGeoCoord();
-        Log.i(LOG_TAG, String.format("MapView onMapViewDragStarted (%f,%f)", mapPointGeo.latitude, mapPointGeo.longitude));
+
     }
 
     @Override
     public void onMapViewDragEnded(MapView mapView, MapPoint mapPoint) {
-        MapPoint.GeoCoordinate mapPointGeo = mapPoint.getMapPointGeoCoord();
-        Log.i(LOG_TAG, String.format("MapView onMapViewDragEnded (%f,%f)", mapPointGeo.latitude, mapPointGeo.longitude));
+
     }
 
     @Override
     public void onMapViewMoveFinished(MapView mapView, MapPoint mapPoint) {
-        MapPoint.GeoCoordinate mapPointGeo = mapPoint.getMapPointGeoCoord();
-        Log.i(LOG_TAG, String.format("MapView onMapViewMoveFinished (%f,%f)", mapPointGeo.latitude, mapPointGeo.longitude));
+
     }
 
     @Override
-    public void onMapViewZoomLevelChanged(MapView mapView, int zoomLevel) {
-        Log.i(LOG_TAG, String.format("MapView onMapViewZoomLevelChanged (%d)", zoomLevel));
+    public void onPOIItemSelected(MapView mapView, MapPOIItem mapPOIItem) {
+
+    }
+
+    @Override
+    public void onCalloutBalloonOfPOIItemTouched(MapView mapView, MapPOIItem mapPOIItem) {
+        Toast.makeText(this, "Clicked " + mapPOIItem.getItemName() + " Callout Balloon", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onCalloutBalloonOfPOIItemTouched(MapView mapView, MapPOIItem mapPOIItem, MapPOIItem.CalloutBalloonButtonType calloutBalloonButtonType) {
+
+    }
+
+    @Override
+    public void onDraggablePOIItemMoved(MapView mapView, MapPOIItem mapPOIItem, MapPoint mapPoint) {
+
     }
 }
